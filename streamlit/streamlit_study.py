03b5ae7da4315b14cf382963ce962f57a5cebfc6
JrJuni <behomely0409@gmail.com>
2025-07-22 21:57:19 +0900

    streamlit start

---
+++ streamlit/streamlit_study.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Streamlit Study",
+    page_icon=":tada:",
+    layout="wide"
+)
+
+st.title("Streamlit Study")
+
+st.write("Heladlo World")
